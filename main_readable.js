433facc8e4adfa5988c928cf0c332d733751b263
stargateprovider <kana46@windowslive.com>
2020-06-24 23:37:27 +0300

    null check

--- main_readable.js
+++ main_readable.js
@@ -243,7 +243,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
 	}
 
 	var loadStaticLinks = result => {
-		if (result && result["staticLinks"]){
+		if (result != null && result["staticLinks"]){
 			jsonFileHandler(result["staticLinks"]);
 		} else {
 			readFile("links.json",
