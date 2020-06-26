451d4d36272ff6c3ec717aafd13d4cb760d4dcb8
stargateprovider <kana46@windowslive.com>
2020-06-26 04:59:01 +0300

    delete fix

--- main_readable.js
+++ main_readable.js
@@ -342,7 +342,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
 		} else {
 			delFeed = e => {
 				let feeds = JSON.parse(localStorage.getItem("feeds"));
-				feeds.pop(e);
+				delete feeds[e];
 				localStorage.setItem("feeds", JSON.stringify(feeds));
 				feednames.style.display = "none";
 			}
